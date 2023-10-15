6c6eaae60085bba1557b7ac2809884fac6ad7ef3
eunannana <95303337+eunannana@users.noreply.github.com>
2023-10-15 21:27:55 +0700

    add connection to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:redlenshoescleaning/view/login.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
